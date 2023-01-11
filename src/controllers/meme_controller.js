bf4edf0ab6e87990ac5b200de9891739f625cc9a
Aurelien49 <92448486+AurelienV49@users.noreply.github.com>
2023-01-11 22:26:00 +0100

    Add log to default page

--- src/controllers/meme_controller.js
+++ src/controllers/meme_controller.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 const express = require("express");
 const cloudinary = require('cloudinary').v2;
 const router = express.Router();
-import fetch from "node-fetch";
+const fetch = require("node-fetch");
 
 exports.getMeme = (req, res, next) => {
     console.log('getMeme: ', req.params.id);
