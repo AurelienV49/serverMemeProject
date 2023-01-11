f114ecc57b5eed89d504781744a590c3e51e83b6
Aurelien49 <92448486+AurelienV49@users.noreply.github.com>
2023-01-11 21:57:29 +0100

    add fetch definition 2 files

--- src/controllers/user_controller.js
+++ src/controllers/user_controller.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken');
 const {OAuth2Client} = require('google-auth-library');
 const l = require('../log/main_logger');
 const sgMail = require('@sendgrid/mail');
+import fetch from "node-fetch";
 
 //
 const CLIENT_ID = process.env.CLIENT_ID;
