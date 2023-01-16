aa9cb53d325d833b863f5b7865fd4c7e87e9ea02
Aurelien49 <92448486+AurelienV49@users.noreply.github.com>
2023-01-16 18:54:44 +0100

    Collapsing NavBar 16-1

--- src/routes/meme_routes.js
+++ src/routes/meme_routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const objectCtrl = require('../controllers/meme_controller');
+const auth = require("../middlewares/auth");
 
-router.get('/bdduser/:id', objectCtrl.getMeme);
-router.get('/memes-user-history/:id', objectCtrl.getMemesUserHistory);
+router.get('/bdduser/:id', [auth], objectCtrl.getMeme);
+router.get('/memes-user-history/:id', [auth], objectCtrl.getMemesUserHistory);
 router.get('/imgflip/', objectCtrl.getMemesFromImgFlip);
-router.post('/createMeme/', objectCtrl.createMeme);
-router.put('/updateMemeBdduser/:id', objectCtrl.updateMeme);
-router.delete('/deleteMemeBdduser/:id', objectCtrl.deleteMeme);
+router.post('/createMeme/', [auth], objectCtrl.createMeme);
+router.put('/updateMemeBdduser/:id', [auth], objectCtrl.updateMeme);
+router.delete('/deleteMemeBdduser/:id', [auth], objectCtrl.deleteMeme);
 
 module.exports = router;
